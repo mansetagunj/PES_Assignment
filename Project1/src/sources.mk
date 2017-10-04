6d541482332fceefeaf35663e3849800b130de83
mansetagunj <gunjmanseta@gmail.com>
2017-10-04 14:33:14 -0600

    Modifying sources.mk

--- Project1/src/sources.mk
+++ Project1/src/sources.mk
@@ -2,8 +2,8 @@ SRCS_COMMON_C= 	conversion.c \
 	     	debug.c \
 	     	memory.c \
 	     	project.c \
-	    	 main.c
+	    	main.c 
 
 SRCS_KL25Z_S=	startup_MKL25Z4.S
 
-
+SRCS_KL25Z_C=	startup_MKL25Z4.c
